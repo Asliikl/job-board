07da9cd2efa5029b8b29b00fc6d42b9304ec10a2
Asliikl <asliikiel9@gmail.com>
2025-01-28 19:40:35 +0300

    Added Laravel Debug Bar integration, Vite-based Tailwind CSS installation, widget usage (Job Card, Label..), breadcrumb navigation and job listing page features such as text search and job filtering by salary range.

---
+++ resources/views/components/link-button.blade.php
@@ -0,0 +1,4 @@
+<a href="{{$href}}"
+   class="rounded-md border border-green-300 bg-green-300 px-2.5 py-1.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-green-100">
+    {{$slot}}
+</a>
