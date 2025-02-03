e5a524290849ea836a97432c4129b86c67982117
Asliikl <asliikiel9@gmail.com>
2025-02-03 14:01:37 +0300

    Added Factory, Seeder, Employer Search, Authentication, and Job Applications

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Employer;
+use App\Models\Job;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,13 +15,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        \App\Models\User::factory()->create([
+            'name' =>'Aslihan ikiel',
+            'email' =>'aslihanikiel@gmail.com',
         ]);
+       \App\Models\User::factory(300)->create();
 
-        \App\Models\Job::factory(100)->create();
+        $users = \App\Models\User::all()->shuffle();
+
+        for ($i=0; $i<20; $i++){
+            \App\Models\Employer::factory()->create([
+                'user_id'=>$users->pop()->id
+            ]);
+        }
+        $employers = \App\Models\Employer::all();
+        for ($i=0; $i<100; $i++){
+            \App\Models\Job::factory()->create([
+                'employer_id'=>$employers->random()->id
+            ]);
+        }
+
+        foreach ($users as $user){
+            $jobs = \App\Models\Job::inRandomOrder()->take(rand(0,4))->get();
+
+            foreach ($jobs as $job){
+                \App\Models\JobApplication::factory()->create([
+                    'job_id' => $job->id,
+                    'user_id'=>$user->id
+                ]);
+            }
+        }
+        //\App\Models\Job::factory(100)->create();
     }
 }
